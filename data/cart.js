91009c2f99136a0a7fa0482acf181e32064677e8
LlewBach <roepjynamy@hotmail.com>
2023-09-22 21:41:04 +0100

    Ch14 Exercises

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,25 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
-  cart = [{
+  cart = [/*{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-  }];
+  }*/];
 }
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart)); 
+}
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  return cartQuantity;
 }
 
 export function addToCart(productId) {
@@ -43,5 +51,18 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
   saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,8 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-
+updateCartQuantity();
 
 let productsHTML = '';
 
@@ -65,12 +65,9 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
 
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.cart-quantity')
     .innerHTML = cartQuantity;
